5fc91a8ad4549d7bf4c9966cd172e1277dde19f0
alex4pascal <113620511+alex4pascal@users.noreply.github.com>
2024-01-16 20:06:50 +0200

    Add files via upload

---
+++ Vehicle.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XF_2023
+{
+    internal class Vehicle
+    {
+        public string engine_type = "gas";  
+
+        
+        protected string color;
+        protected string name;
+        protected int maxSpeed;  
+        protected int year;
+
+        
+        public string Name
+        { 
+            get { return name; }
+            set { name = value; }
+        }
+        public string Color
+        {
+            get { return color; }
+            set { color = value; }
+        }
+
+        public int Year
+        {
+            get { return year; }
+           
+            set
+            {
+                if (value < 1980)
+                {
+                    throw new Exception("Please pass a correct value");
+                }
+                else
+                {
+                    year = value;
+                }
+            }
+        }
+
+
+        public void honk()             // Vehicle method 
+        {
+            Console.WriteLine("Tuut, tuut!");
+        }
+    }
+}
